7a403641ce0cfba809fbea5bfa9e0d074889d88d
Trguc <treyguc@gmail.com>
2018-10-30 17:58:21 -0700

    Hi I did this -Trey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamLibs/revHubIMUGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamLibs/revHubIMUGyro.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.teamLibs;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 
+import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -12,25 +13,40 @@ public class revHubIMUGyro {
 
     private Telemetry telemetry;
     private BNO055IMU imu;
-    private Float currentDirection;
-
+    private int currentDirection;
+    float currentHeading;
     Orientation anglesCurrent;
+    Orientation anglesLast;
+
+
+
 
     revHubIMUGyro (BNO055IMU theimu,Telemetry theTelemetry){
         telemetry = theTelemetry;
         imu = theimu;
         currentDirection = 0;
+        // set up our IMU
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imu.initialize(parameters);
     }
 
 
     float resetHeading() {
+        currentHeading = 0;
+        anglesLast = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
-
+        return currentHeading;
     }
 
     float getHeading() {
         anglesCurrent = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        anglesCurrent.firstAngle
+        telemetry.addData("Heading", anglesCurrent.firstAngle);
+        anglesLast = anglesCurrent;
 
+        return currentHeading;
     }
 
 
