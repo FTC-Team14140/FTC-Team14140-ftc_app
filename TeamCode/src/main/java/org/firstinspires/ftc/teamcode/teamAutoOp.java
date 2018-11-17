856941479ccb436e175de2deaec7c60fe2b3f634
lizzieyoon <lizzieyoon@icloud.com>
2018-11-17 11:28:44 -0800

    outline for autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamAutoOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.teamLibs.linearActuater;
+
+@TeleOp(name="Team Auto", group="Linear Opmode")
+public class teamAutoOp extends LinearOpMode {
+    private linearActuater La;
+
+
+    @Override
+    public void runOpMode() {
+        //initialization code
+
+        // Wait for the game to start (drver presses PLAY)
+        waitForStart();
+        La.lowerRobot();
+
+    }
+    public void runOpmode() {
+        waitForStart();
+        La.retractFully();
+    }
+}
