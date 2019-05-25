d8ab6972d174abf0bae6a5d1599d7656e1ef4b01
roboticslab <devin@breise.com>
2019-05-25 16:56:54 -0700

    Have completed the code to the roboic arm for basic human controled movement movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
@@ -6,6 +6,8 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.teamLibs.goBuildAServo2000;
+
 
 import org.firstinspires.ftc.teamcode.teamLibs.HiTecServo;
 import org.firstinspires.ftc.teamcode.teamLibs.basicMovement;
@@ -16,30 +18,76 @@ public class ArmTest extends LinearOpMode {
     private DcMotor BaseMotor;
     private DcMotor joint2Motor;
     private HiTecServo TurnTable;
-    private Servo ttServo;
-    private basicMovement basicMove;
+    private goBuildAServo2000 grabber;
 
     public void runOpMode() {
         teamUtil.log("Gettign motor 0 from hardware map");
         BaseMotor = hardwareMap.get(DcMotor.class, "motor0");
         teamUtil.log("Gettign motor 1 from hardware map");
         joint2Motor = hardwareMap.get(DcMotor.class, "motor1");
-        basicMove = new basicMovement(BaseMotor, joint2Motor, hardwareMap.get(BNO055IMU.class,"imu"), telemetry);
+        grabber = new goBuildAServo2000(hardwareMap.get(Servo.class, "Servo1"), telemetry);
         TurnTable = new HiTecServo(hardwareMap.get(Servo.class, "Servo0"), telemetry);
+
+        int gDegrees = 215; // start open
+        int tDegrees = 75; // start at middle
+        TurnTable.goTo(tDegrees);
+        grabber.goTo(gDegrees);
+
+        joint2Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        joint2Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
         telemetry.addData("Status", "Waiting for Start...");
         telemetry.update();
-
-        TurnTable.goTo(0);
-
         waitForStart();
 
-        if (gamepad2.right_bumper){
-            BaseMotor.setPower(0.5);
-        } else if (gamepad2.left_bumper){
-            BaseMotor.setPower(-0.5);
-        } else {
-            BaseMotor.setPower(0);
-        }
+        while (opModeIsActive()) {
 
+            //base motor
+            if (gamepad2.right_bumper) {
+                BaseMotor.setPower(0.5);
+                telemetry.addData("moving base", .5);
+            } else if (gamepad2.left_bumper) {
+                BaseMotor.setPower(-0.5);
+                telemetry.addData("moving base", -.5);
+            } else {
+                BaseMotor.setPower(0);
+            }
+
+            //arm motor
+            if (gamepad2.x) {
+                joint2Motor.setPower(0.3);
+                telemetry.addData("moving arm", .3);
+            } else if (gamepad2.b) {
+                joint2Motor.setPower(-0.3);
+                telemetry.addData("moving arm", -.3);
+            } else {
+                joint2Motor.setPower(0);
+            }
+
+            //grabber Servo
+            if (gamepad2.dpad_right) {
+                grabber.goTo(250);
+            } else if (gamepad2.dpad_left) {
+                grabber.goTo(215);
+            }
+
+            //turn table
+            if (gamepad2.a) {
+                tDegrees--;
+                TurnTable.goTo(tDegrees);
+                teamUtil.sleep(50);
+            } else if (gamepad2.y){
+                tDegrees++;
+                TurnTable.goTo(tDegrees);
+                teamUtil.sleep(50);
+            }
+
+
+            telemetry.addData("gDegrees", gDegrees);
+            telemetry.addData("tDegrees", tDegrees);
+            telemetry.update();
+
+        }
     }
 }
